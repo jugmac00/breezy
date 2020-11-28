ced7ecb7ec6408f3afbb5d295894c00321e35646
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-11-28 04:00:00 +0000

    Revert "Fix return value."
    This reverts commit 84ab1f3f8863682fa246b45e50797416f5c197e4.

--- breezy/git/interrepo.py
+++ breezy/git/interrepo.py
@@ -738,7 +738,7 @@ class InterLocalGitLocalGitRepository(InterGitGitRepository):
 
         (pack_hint, last_rev, remote_refs) = self.fetch_objects(
             determine_wants, limit=limit, lossy=lossy)
-        return {}
+        return None
 
     def fetch_objects(self, determine_wants, mapping=None, limit=None,
                       lossy=False):
